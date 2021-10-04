6f667209776b06029624c5cce001218a32d8144e
Louis Ng <kamchuenlouisng@gmail.com>
2021-10-04 05:43:09 -0400

    can now safe nested arrays

--- api/controllers/post/create.js
+++ api/controllers/post/create.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
 
 
@@ -8,15 +10,24 @@ module.exports = {
 
 
   inputs: {
-    title:{
-      description:'Title of the post object',
-      type:'string',
-      required: true,
+    // title:{
+    //   description:'Title of the post object',
+    //   type:'string',
+    //   required: true,
       
-    },
-    postBody:{
+    // },
+    // postBody:{
+    //   type:'string',
+    //   required: true,
+      
+    // },
+    embedCode:{
       type:'string',
       required: true,
+    },
+    title:{
+      type:'string',
+      required: true
       
     }
   },
@@ -28,11 +39,21 @@ module.exports = {
 
 
   fn: async function (inputs) {
+    const a = await Company.find({name: "Scotia iTRADE"})
+    
     console.log('we are now inside post/create action')
     const userId = this.req.session.userId
+    console.log('userid')
     console.log(userId)
+    console.log('fetch company')
+    console.log(a)
+    let embedB = {"en-us":inputs.embedCode}
+    let embedArray = [];
+    embedArray.push(embedB)
     //console.log(this.req.me)
-    await Post.create({title: inputs.title, body: inputs.postBody, user: userId})
+    // await Post.create({title: inputs.title, body: inputs.postBody, user: userId})
+     //await Ad.create({title: inputs.title,company:a[0].id, embedCode: '[{"en-us":"'+ inputs.embedCode+'"}]', title: '[{"en-us":"'+ inputs.embedCode+'"}]' })
+     await Ad.create({title: inputs.title,company:a[0].id, embedCode: embedArray, title: '[{"en-us":"'+ inputs.embedCode+'"}]' })
     this.res.redirect('/home')
     //return;
 
